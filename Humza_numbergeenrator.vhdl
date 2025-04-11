1e9f1ea8d76f7254f2025caea64a3c042d7f603a
Humza Rana <142959387+HumzaProfessional@users.noreply.github.com>
2025-04-11 09:31:42 -0400

    Update Humza_numbergeenrator.vhdl

--- Humza_numbergeenrator.vhdl
+++ Humza_numbergeenrator.vhdl
@@ -6,20 +6,27 @@ entity lfsr_random_gen is
    Generic: 
     Port ( clock : in STD_LOGIC;
            reset : in STD_LOGIC;
-          button: in STD_LOGIC;
-           random_num0 : out STD_LOGIC_VECTOR(7 downto 0));
-           random_num1 : out STD_LOGIC_VECTOR(7 downto 0));
-           random_num2 : out STD_LOGIC_VECTOR(7 downto 0));
+           button: in STD_LOGIC;
+           
+           random_number: out STD_LOGIC_VECTOR(7 downto 0)
+         )
           
-          
+  ----------------------------state machine declaration------------------------------------------------------------
+type State_t is (BLANK, GEN, 
+signal currentState: State_t;
+signal nextState: State_t;
+                 
 end lfsr_random;
 
-architecture random_gen_ARCH  of lfsr_random is
+architecture random_gen_ARCH  of lfsr_random_gen is
+
+   
+   ---------------------------signals---------------------------------------------------------------------
     signal lfsr_reg : STD_LOGIC_VECTOR(7 downto 0) := "10101010"; -- Seed
 begin
     process(clock, reset)
     begin
-        if rst = '1' then
+        if reset = '1' then
             lfsr_reg <= "10101010"; -- Reset to initial seed
         elsif rising_edge(clk) then
             lfsr_reg <= lfsr_reg(6 downto 0) & (lfsr_reg(7) xor lfsr_reg(5));
@@ -30,7 +37,6 @@ begin
   if (reset = ACTIVE) then
      dataOUT <= (others => '0');
   elsif (risingedge(clock)) then
-    
     rnd <= lfsr_reg-ARCH;
 end Behavioral;
 
